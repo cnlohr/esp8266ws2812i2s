33019b811b7aed4f5dbbf738e1db9c8be6904fe8
Charles Lohr <charles@cnlohr.com>
2016-08-29 00:17:05 -0400

    Fix up timing issues

--- user/user_main.c
+++ user/user_main.c
@@ -18,6 +18,7 @@
 #define procTaskQueueLen    1
 
 static volatile os_timer_t some_timer;
+static volatile os_timer_t pattern_timer;
 static struct espconn *pUdpServer;
 uint8_t last_leds[512*3] = {0};
 int last_led_count = 0;
@@ -51,23 +52,26 @@ static void ICACHE_FLASH_ATTR procTask(os_event_t *events)
 	system_os_post(procTaskPrio, 0, 0 );
 }
 
-//Timer event.
-static void ICACHE_FLASH_ATTR myTimer(void *arg)
+static void ICACHE_FLASH_ATTR patternTimer(void *arg)
 {
-	CSTick( 1 );
-
     if(pattern == PATTERN_NONE) return;
 
     int it;
     for(it=0; it<last_led_count; ++it) {
-        uint32_t hex = hex_pattern( pattern, it, last_led_count, frame*80 );
+        uint32_t hex = hex_pattern( pattern, it, last_led_count, frame );
         last_leds[3*it+0] = (hex>>8);
         last_leds[3*it+1] = (hex);
         last_leds[3*it+2] = (hex>>16);
     }
-    frame += ws_sleep/80;
+    frame ++;
     debug("Frame: %i", (int)frame);
     ws2812_push( (char*)last_leds, 3*last_led_count);
+}
+
+//Timer event.
+static void ICACHE_FLASH_ATTR myTimer(void *arg)
+{
+	CSTick( 1 );
 }
 
 
@@ -147,6 +151,11 @@ void user_init(void)
 	os_timer_disarm(&some_timer);
 	os_timer_setfn(&some_timer, (os_timer_func_t *)myTimer, NULL);
 	os_timer_arm(&some_timer, 100, 1);
+
+	//Pattern Timer example
+	os_timer_disarm(&pattern_timer);
+	os_timer_setfn(&pattern_timer, (os_timer_func_t *)patternTimer, NULL);
+	os_timer_arm(&pattern_timer, 10, 1);
 
 	ws2812_init();
 
